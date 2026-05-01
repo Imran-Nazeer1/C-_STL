072f7ec65d1df43509f7d6f1e0c1fd0eba9603e5
Imran-nazeer1 <imranofficial1106@gmail.com>
2026-05-01 09:39:42 +0530

    Add Array

--- Pair.cpp
+++ Pair.cpp
@@ -5,6 +5,10 @@ int main(){
     cout<<p.first<<" "<<p.second<<endl;
 
     pair<int , pair<int , int >> q= {2,{4,6}};
-    cout<<q.first<<" "<<q.second.first<<" "<<q.second.second;
-    
+    cout<<q.first<<" "<<q.second.first<<" "<<q.second.second<<endl;
+
+    // pair array
+
+    pair<int ,int > arr[]={{1,2},{3,4}};
+    cout<<arr[1].first;
 }
